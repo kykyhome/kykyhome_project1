db6968ef996c6206660fce1300c0e79fce97ae90
kykyhome <tkoinng@gmail.com>
2024-07-21 12:58:24 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,13 +2,14 @@ basic.showIcon(IconNames.Heart)
 basic.pause(1000)
 basic.showString("" + (input.temperature()))
 basic.pause(1000)
+let Pressed = 0
 basic.forever(function () {
-    if (input.buttonIsPressed(Button.A)) {
+    if (input.buttonIsPressed(Button.A) || Pressed == 0) {
         basic.showString("" + (input.temperature()))
-        basic.pause(1000)
+        Pressed = 0
     }
-    if (input.buttonIsPressed(Button.B)) {
+    if (input.buttonIsPressed(Button.B) || Pressed == 1) {
         basic.showString("" + (input.lightLevel()))
-        basic.pause(1000)
+        Pressed = 1
     }
 })
