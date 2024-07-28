1689fa48b56aa416f0f02d073bdfdea7cde7dd37
kykyhome <tkoinng@gmail.com>
2024-07-28 14:01:04 +0800

    temp monitor

--- main.ts
+++ main.ts
@@ -1,15 +1,14 @@
+let Beepnumber = 0
 basic.showIcon(IconNames.Heart)
 basic.pause(1000)
-basic.showString("" + (input.temperature()))
-basic.pause(1000)
-let Pressed = 0
 basic.forever(function () {
-    if (input.buttonIsPressed(Button.A) || Pressed == 0) {
-        basic.showString("" + (input.temperature()))
-        Pressed = 0
+    basic.showNumber(input.temperature())
+    Beepnumber = 0
+    if (input.temperature() >= 29) {
+        Beepnumber = input.temperature() - 30
     }
-    if (input.buttonIsPressed(Button.B) || Pressed == 1) {
-        basic.showString("" + (input.lightLevel()))
-        Pressed = 1
+    for (let Index = 0; Index <= Beepnumber; Index++) {
+        music.play(music.createSoundExpression(WaveShape.Square, 400, 600, 255, 0, 100, SoundExpressionEffect.Warble, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
     }
+    basic.pause(1000)
 })
