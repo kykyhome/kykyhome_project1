2a2269c6720cb12b71822ae253ade1684e9a0fb5
kykyhome <tkoinng@gmail.com>
2024-07-28 14:54:52 +0800

    Restore '20/7/2024 下午9:05:07 c1'

--- main.ts
+++ main.ts
@@ -1,14 +1,4 @@
-let Beepnumber = 0
 basic.showIcon(IconNames.Heart)
-basic.pause(1000)
 basic.forever(function () {
-    basic.showNumber(input.temperature())
-    Beepnumber = 0
-    if (input.temperature() >= 29) {
-        Beepnumber = input.temperature() - 30
-    }
-    for (let Index = 0; Index <= Beepnumber; Index++) {
-        music.play(music.createSoundExpression(WaveShape.Square, 400, 600, 255, 0, 100, SoundExpressionEffect.Warble, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
-    }
-    basic.pause(1000)
+    basic.showString("Hello!")
 })
